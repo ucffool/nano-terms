4a97c531b53257d1439b3ac80ec8d5e0cab9d8bd
Mario Lurig <ucffool@gmail.com>
2025-03-28 15:11:39 -0600

    capitalize duh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,7 @@
     </div>
 </main>
 <div class="w-full border-t-2 px-2 border-black flex justify-between items-end leading-none">
-    <div class="mb-1">Last Updated: <b>{timeAgo(definitions.last_updated)}</b></div>
+    <div class="mb-1">Last Updated: <b class="capitalize">{timeAgo(definitions.last_updated)}</b></div>
     <a class="py-1" href="https://github.com/ucffool/nano-terms" aria-label="GitHub Project" title="NanoTerms GitHub Project">
         <img src={imgGithub} alt="GitHub Logo" class="w-5 h-auto"/>
     </a>
